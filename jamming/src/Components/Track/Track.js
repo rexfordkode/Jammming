e1cbc0415d6b43e8f5309ff14af9957ee43a9bbf
rexfordkode <86994591+rexfordkode@users.noreply.github.com>
2021-09-13 15:34:38 +0100

    second commit up to 34

--- jamming/src/Components/Track/Track.js
+++ jamming/src/Components/Track/Track.js
@@ -1,8 +1,29 @@
-<div class="Track">
-    <div class="Track-information">
-      <h3><!-- track name will go here --></h3>
-      <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+import React from "react";
+import './Track.css';
+
+class Track extends React.Component {
+  
+  renderAction (){
+    if(this.props.isRemoval){
+      return  <button className="Track-action">-</button>
+    } else{
+      return <button className="Track-action">+</button>
+    }
+  }
+  render(){
+
+    return(
+      <div className="Track">
+      <div className="Track-information">
+    <h3></h3>
+    <p></p>
     </div>
-    <button class="Track-action"><!-- + or - will go here --></button>
-  </div>
-  Please login or signup to FixMe
+      {this.renderAction()}
+    </div>
+    )
+
+    
+  }
+}
+
+export default Track;
